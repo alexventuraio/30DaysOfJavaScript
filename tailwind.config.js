281624fc1d8608ce44ca8a823b4c0bc4f7287db3
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2021-10-02 13:16:55 -0500

    Add config for Yarn, TailwindCSS and PostCSS.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ["./examples/**/*.html", "./index.html"]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
